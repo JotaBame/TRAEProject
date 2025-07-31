c0d8f3de928820fabd0120b18880d94f89a12f8f
Horseyboi <69808736+Horseyboi@users.noreply.github.com>
2025-07-30 21:10:19 -0400

    Simplified plantera check for eclipse; fixes incompat with 3 days mod

--- Changes/ILEclipse.cs
+++ Changes/ILEclipse.cs
@@ -9,9 +9,12 @@ namespace TRAEilHooks {
             ILCursor c = new(il);
 
             // Just go to downedMechBossAny it only appears here once
-            c.GotoNext(MoveType.After,
+			c.GotoNext(MoveType.After,
+				x => x.MatchStsfld(typeof(Terraria.Main).GetField("eclipse"))
+			);
+            c.GotoPrev(MoveType.After,
                 x => x.MatchLdsfld(typeof(Terraria.NPC).GetField("downedMechBossAny"))
-                );
+			);
 
             c.Emit(OpCodes.Ldsfld, typeof(Terraria.NPC).GetField("downedPlantBoss"));
             c.Emit(OpCodes.Or);
