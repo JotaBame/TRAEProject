f5b77b1d847c9ead6625078af4388ef6a1348bbe
Horseyboi <69808736+Horseyboi@users.noreply.github.com>
2025-07-30 21:02:56 -0400

    Simplified eclipse Plantera check; fixes incompat with 3 days mod

--- Changes/ILEclipse.cs
+++ Changes/ILEclipse.cs
@@ -8,26 +8,13 @@ namespace TRAEilHooks {
         internal static void DoStuff(ILContext il) {
             ILCursor c = new(il);
 
-            // Jump to after the Eclipse check; this is a bit long and I don't think anything else resets the dial cooldowns here, but I'm being safe
-            c.GotoNext(MoveType.Before,
-                x => x.MatchLdcI4(0),
-                x => x.MatchStsfld("Terraria.Main","sundialCooldown"),
-                x => x.MatchLdcI4(0),
-                x => x.MatchStsfld("Terraria.Main", "moondialCooldown"),
-                x => x.MatchLdcI4(1),
-                x => x.MatchStsfld("Terraria.Main", "eclipse")
+            // Just go to downedMechBossAny it only appears here once
+            c.GotoNext(MoveType.After,
+                x => x.MatchLdsfld(typeof(Terraria.NPC).GetField("downedMechBossAny"))
                 );
 
-            
-            //ILLabel eclipseLabel = c.MarkLabel(); // Save this instruction for later
-            c.Index -= 6; // Jump back to where we want to add a condition
             c.Emit(OpCodes.Ldsfld, typeof(Terraria.NPC).GetField("downedPlantBoss"));
-            c.Index += 1;
             c.Emit(OpCodes.Or);
-
-
-
-            //c.Emit(OpCodes.Brtrue, eclipseLabel);
 
             return;
         }
